7c15267a3fd3778018dc7f149e5a430204cbda00
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-06-26 18:56:32 -0700

    js image function added

--- js/main.js
+++ js/main.js
@@ -26,14 +26,15 @@ var newT = document.createTextNode('Success!');
 
 
 // No,12, 13
-
-//create class img to HTML
-var myImg = document.createElement('img');
 //create class button to HMTL
 var myBtn = document.createElement('button');
+//create class img to HTML
+var myImg = document.createElement('img');
+
  //create class to button created above
  myBtn.className = "btn btn-outline-info btn-lg";
-
+ //create class to img
+ myImg.className = 'img-fluid';
 //create text to button created above
 var t = document.createTextNode("click!");
 
@@ -44,7 +45,35 @@ var t = document.createTextNode("click!");
  //append text(t) to button(myBtn).Create text inside buttton
   myBtn.appendChild(t);
 
-// var image {
-//   path: 'https://i.pinimg.com/originals/5b/5c/c2/5b5cc265cf4135803a46b3a210120f5b.jpg',
-//   desc : 'lion'
+//create image array/object.
+//we will change img by clicking btn later
+var images = [
+  {
+  path : 'https://i.pinimg.com/originals/5b/5c/c2/5b5cc265cf4135803a46b3a210120f5b.jpg',
+  desc: 'lion',
+ },
+ {
+   path : 'https://icdn2.digitaltrends.com/image/elephant-head-on.jpg',
+   desc: 'elephant'
+ }
+];
+
+//create onclick function to myBtn created above
+myBtn.onclick = function () {myFunction()};
+// store counter as 0, we will use counter to change image
+var counter = 0;
+
+// function myFunction() {
+//   myImg.src = images[0].path;
 // };
+
+//images change when you click btn.
+function myFunction() {
+  //countaer increases by one everytime when function run(when you click btn)
+  counter++ ;
+  if(counter >= images.length) {
+    counter = 0;
+  }
+  myImg.src = images[counter].path;
+  myImg.alt = images[counter].desc;
+};
